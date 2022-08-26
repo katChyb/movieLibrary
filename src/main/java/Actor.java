d4df475aa7741fb471d178c9d060c908d1862d63
katChyb <64010913+katChyb@users.noreply.github.com>
2022-08-26 08:56:39 +0200

    26-/08

---
+++ src/main/java/Actor.java
@@ -0,0 +1,10 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Actor {
+    private String name;
+    private String surname;
+
+}
---
+++ src/main/java/Movie.java
@@ -0,0 +1,16 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public class Movie {
+    private String title;
+    private String genre;
+    private Director director;
+    private List<Actor> actors;
+    private int publishingDate;
+
+
+}
